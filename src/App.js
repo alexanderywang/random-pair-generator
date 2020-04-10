6d77277fcee30ab099934b0e1f7fed67e368c4d2
Alexander Wang <alexander.wang22@gmail.com>
2020-04-10 00:26:24 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      grads: [
+        "Alex",
+        "Leslie",
+        "Thomas",
+        "Yan",
+        "April",
+        "Max",
+        "Mark",
+        "Natasha",
+        "Mike",
+        "Peter",
+        "Nelson",
+        "Manny"
+      ],
+      pairs: []
+    };
+  }
+
+  randomize = () => {
+    let array = this.state.grads;
+    for (let i = 0; i < array.length - 1; i++) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+
+    this.splitPairs(array);
+  };
+
+  splitPairs(array) {
+    let pairs = [];
+
+    for (let i = 0; i < array.length; i += 2) {
+      let pair = array.slice(i, i + 2);
+      pairs.push(pair);
+    }
+
+    this.setState({ pairs: pairs });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Full Stack RANDOM Algo Pairs</h2>
+
+        <button onClick={this.randomize}>Make pairs</button>
+        {this.state.pairs.map((pair, i) => (
+          <p>
+            <span>Pair {i + 1}:</span> {pair.join(" ")}
+          </p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
